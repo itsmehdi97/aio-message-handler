f17ded05f1cd9056630fccba6778389cf609c68c
itsmehdi97 <itsmehdi97@gmail.com>
2021-12-17 11:50:28 +0330

    init version

---
+++ aio_message_handler/version.py
@@ -0,0 +1,11 @@
+author_info = (("Mehdi Kamani", "itsmehdi97@gmail.com"),)
+
+package_info = "Wraper around the aio-pika for consumining messages."
+package_license = "MIT Software License"
+
+team_email = "me@mosquito.su"
+
+version_info = (0, 0, 1)
+
+__author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
+__version__ = ".".join(map(str, version_info))
