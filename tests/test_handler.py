798167b88b85815ff61f2b8a93397c351e2dedf4
mehdi <kamani@ipreez.ir>
2022-04-10 18:01:31 +0430

    test handler stop

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -18,6 +18,13 @@ def handler():
     )
 
 
+@pytest.fixture
+def running_handler(monkeypatch, handler: Handler):
+    handler._ctag = "ctag"
+    handler.queue = mock.Mock(name="queue")
+    return handler
+
+
 @pytest.mark.asyncio
 async def test_handler_start(create_mock_coro, mock_conn, handler: Handler):
     mock_setup, _ = create_mock_coro(
@@ -36,3 +43,81 @@ async def test_handler_start(create_mock_coro, mock_conn, handler: Handler):
     assert mock_setup.call_count == 1
     assert handler._ctag == mock_consume.return_value
     mock_consume.assert_called_once_with(handler.cb)
+
+
+@pytest.mark.asyncio
+async def test_handler_stop(
+    create_mock_coro,
+    running_handler: Handler
+):
+    mock_cancel, coro = create_mock_coro()
+    running_handler.queue.cancel = coro
+
+    await running_handler.stop(timeout='timeout', nowait='nowait')
+
+    mock_cancel.assert_called_once_with(
+        running_handler._ctag, timeout='timeout', nowait='nowait')
+
+
+@pytest.mark.asyncio
+async def test_handler_with_exchange_setup(
+    create_mock_coro,
+    handler: Handler
+):
+    mock_channel = mock.Mock()
+    mock_declare_q, mock_channel.declare_queue = create_mock_coro()
+    mock_declare_exc, mock_channel.declare_exchange = create_mock_coro()
+    mock_set_qos, mock_channel.set_qos = create_mock_coro()
+
+    mock_declare_exc.return_value = 'exchange'
+    mock_queue = mock.Mock(name="queue")
+    mock_declare_q.return_value = mock_queue
+    mock_bind, mock_queue.bind = create_mock_coro()
+
+    result = await handler.setup(mock_channel)
+
+    assert result == mock_queue
+    assert handler._ready is True
+    mock_declare_exc.assert_called_once_with(handler._exchange)
+    mock_declare_q.assert_called_once_with(handler._queue)
+    mock_bind.assert_called_once_with(
+        mock_declare_exc.return_value, routing_key=handler._binding_key)
+    mock_set_qos.assert_called_once_with(prefetch_count=handler.prefetch_count)
+
+
+@pytest.mark.asyncio
+async def test_handler_without_exchange_setup(
+    create_mock_coro,
+    handler: Handler
+):
+    handler._exchange = None
+
+    mock_channel = mock.Mock()
+    mock_declare_q, mock_channel.declare_queue = create_mock_coro()
+    mock_declare_exc, mock_channel.declare_exchange = create_mock_coro()
+    mock_set_qos, mock_channel.set_qos = create_mock_coro()
+
+    mock_declare_exc.return_value = 'exchange'
+    mock_queue = mock.Mock(name="queue")
+    mock_declare_q.return_value = mock_queue
+    mock_bind, mock_queue.bind = create_mock_coro()
+
+    result = await handler.setup(mock_channel)
+
+    assert result == mock_queue
+    assert mock_declare_exc.call_count == 0
+    assert mock_bind.call_count == 0
+    assert handler._ready is True
+    mock_declare_q.assert_called_once_with(handler._queue)
+    mock_set_qos.assert_called_once_with(prefetch_count=handler.prefetch_count)
+
+
+def test_repr(handler: Handler):
+    names = [
+        handler._name,
+        handler._queue,
+        handler._exchange,
+        handler._binding_key]
+    result = repr(handler)
+    for name in names:
+        assert name in result
